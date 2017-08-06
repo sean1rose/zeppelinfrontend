6b30568e5310b209ec537c7d49d3e163d095419c
Sean Rose <sean1rose@gmail.com>
2017-08-05 23:21:19 -0700

    initial commit, based off reactstarterpack, add eslint and webpackDefinePlugin w/ .env

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'zeppelinfrontend';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+
+console.log('CAN U SEE THIS foo?');
